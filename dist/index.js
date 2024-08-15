1b8a598afc90ac118c32409c0ebb65aeced478e2
gerna-v1 <gerardoelimar2@gmail.com>
2024-08-14 21:43:55 -0400

    tried something

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,22 @@
 var poke;
+var div = document.querySelector('#container');
 
 const cargarPaises = () => {
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=60')
+    axios.get('https://pokeapi.co/api/v2/pokemon')
     .then(function (response) {
         // handle success
         console.log(response);
         poke = response.data.results;
+        div.innerHTML = JSON.stringify(poke);
     })
     .catch(function (error) {
         console.log(error);
     });
-    };    
-
+};    
 
 document.addEventListener('DOMContentLoaded', () => {
+    div = document.querySelector('#container');
     cargarPaises();
 })
+    
+
